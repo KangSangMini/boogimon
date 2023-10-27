66f14e38be549d8acedcb140be7c4d5a7e5cdf6f
Lilium <LilyLillyLilium@gmail.com>
2023-10-27 17:37:37 +0900

    Feat: Add Data Object models

---
+++ boogimonProject/src/main/java/model/PlaceDetailDO.java
@@ -0,0 +1,107 @@
+package model;
+
+public class PlaceDetailDO {
+	private String name;
+	private String addr;
+	private String tel;
+	private String detail;
+	private String pay;
+	private String traffic;
+	private String img;
+	private String homepage;
+	private String open;
+	private String close;
+	private String facility;
+	
+	public PlaceDetailDO() {
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddr() {
+		return addr;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+
+	public String getPay() {
+		return pay;
+	}
+
+	public void setPay(String pay) {
+		this.pay = pay;
+	}
+
+	public String getTraffic() {
+		return traffic;
+	}
+
+	public void setTraffic(String traffic) {
+		this.traffic = traffic;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public String getHomepage() {
+		return homepage;
+	}
+
+	public void setHomepage(String homepage) {
+		this.homepage = homepage;
+	}
+
+	public String getOpen() {
+		return open;
+	}
+
+	public void setOpen(String open) {
+		this.open = open;
+	}
+
+	public String getClose() {
+		return close;
+	}
+
+	public void setClose(String close) {
+		this.close = close;
+	}
+
+	public String getFacility() {
+		return facility;
+	}
+
+	public void setFacility(String facility) {
+		this.facility = facility;
+	}
+}
---
+++ boogimonProject/src/main/java/model/StampbookDO.java
@@ -0,0 +1,96 @@
+package model;
+
+import java.util.ArrayList;
+
+public class StampbookDO {
+	private int stampbookId;
+	private String title;
+	private String description;
+	private String nickname;
+	private String stampbookRegdate;
+	private int likeCount;
+	private boolean liked;
+	private ArrayList<StampDO> stampList;
+	private ArrayList<CommentDO> commentList;
+	
+	public StampbookDO() {
+		
+	}
+
+	public int getStampbookId() {
+		return stampbookId;
+	}
+
+	public void setStampbookId(int stampbookId) {
+		this.stampbookId = stampbookId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getStampbookRegdate() {
+		return stampbookRegdate;
+	}
+
+	public void setStampbookRegdate(String stampbookRegdate) {
+		this.stampbookRegdate = stampbookRegdate;
+	}
+
+	public int getLikeCount() {
+		return likeCount;
+	}
+
+	public void setLikeCount(int likeCount) {
+		this.likeCount = likeCount;
+	}
+
+	public boolean getLiked() {
+		return liked;
+	}
+
+	public void setLiked(int isLike) {
+		if(isLike == 1) {
+			this.liked = true;
+		}
+		else {
+			this.liked = false;
+		}
+	}
+
+	public ArrayList<StampDO> getStampList() {
+		return stampList;
+	}
+
+	public void setStampList(ArrayList<StampDO> stampList) {
+		this.stampList = stampList;
+	}
+
+	public ArrayList<CommentDO> getCommentList() {
+		return commentList;
+	}
+
+	public void setCommentList(ArrayList<CommentDO> commentList) {
+		this.commentList = commentList;
+	}
+}
