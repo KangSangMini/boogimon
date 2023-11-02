08eb280add71c7b4b4be6a7faf01801a97b0a99a
Lilium <LilyLillyLilium@gmail.com>
2023-11-02 10:04:38 +0900

    Feat: Add resultCode(99)

--- boogimonProject/src/main/java/model/HeaderJsonWriter.java
+++ boogimonProject/src/main/java/model/HeaderJsonWriter.java
@@ -12,6 +12,7 @@ public class HeaderJsonWriter {
 		this.codeMap = new HashMap<Integer, String>();
 		
 		codeMap.put(0, "NORMAL_CODE");
+		codeMap.put(99, "UNKNOWN_ERROR");
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -19,8 +20,20 @@ public class HeaderJsonWriter {
 		JSONObject headerObj = new JSONObject();
 		
 		// resultCode에 맞는 헤더 생성
-		headerObj.put("resultCode", String.format("%2d", resultCode));
+		headerObj.put("resultCode", String.format("%02d", resultCode));
 		headerObj.put("resultMsg", codeMap.get(resultCode));
+		
+		return headerObj;
+	}
+	
+	/** 임의 코드와 메시지 생성 */
+	@SuppressWarnings("unchecked")
+	public JSONObject getHeaderJsonObj(int resultCode, String msg) {
+		JSONObject headerObj = new JSONObject();
+		
+		// resultCode에 맞는 헤더 생성
+		headerObj.put("resultCode", String.format("%02d", resultCode));
+		headerObj.put("resultMsg", msg);
 		
 		return headerObj;
 	}
